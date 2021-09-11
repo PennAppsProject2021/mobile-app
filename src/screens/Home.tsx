de7c1a74fd59c732fd55c93fd4a7f30cc6713126
Ryan Ziegler <rzig408@gmail.com>
2021-09-11 16:17:04 -0500

    add qr scanner

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import HeaderText from '../components/HeaderText';
 import { useUserData } from '../context/UserDataProvider';
 import { measures, colors } from '../styles';
 import BodyText from '../components/BodyText';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 function Home() {
     const {userData} = useUserData();
+    
+    useEffect(() => {
+        // Poll data from Noah
+    }, [])
+
     return (
         <View style={styles.container}>
             <HeaderText>
@@ -16,6 +22,11 @@ function Home() {
                 When you're ready to check in to your appointment,
                 center your provider's QR code below.
             </BodyText>
+            <View style={styles.scannerView}>
+                {/* <QRCodeScanner
+                    onRead={(data) => {console.log(data)}}
+                /> */}
+            </View>
         </View>
     )
 }
@@ -27,5 +38,11 @@ const styles = StyleSheet.create({
         padding: measures.margins.gutter,
         backgroundColor: colors.bgLight,
         flex: 1
+    },
+    scannerView: {
+        marginTop: measures.margins.gutter,
+        borderRadius: measures.borderRadius,
+        overflow: "hidden",
+        flexGrow: 1
     }
 })
